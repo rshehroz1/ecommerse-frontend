0bacfacc19092d9e58930ee13a67e3b798a4d9e1
Shehroz <raxmatshehroz@gmail.com>
2023-03-15 22:45:08 +0500

    finish

--- src/components/screens/Home.jsx
+++ src/components/screens/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
           <img src="https://images.unsplash.com/photo-1623509434589-7b70883e8d3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d2FscGFwZXJ8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" />
         </div>
         <div className="card-content">
-          <i className="material-icons">favourite</i>
+        <i class="material-icons">favorite</i>
           <h6>Title</h6>
           <p>It is my first post</p>
           <input type="text" placeholder="add comment" />
