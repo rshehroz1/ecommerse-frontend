a696e587b3a2131fccd9164397199dd0f71a2e48
Shehroz <raxmatshehroz@gmail.com>
2023-03-15 22:40:17 +0500

    home card

--- src/components/screens/Home.jsx
+++ src/components/screens/Home.jsx
@@ -6,6 +6,23 @@ export default function Home() {
         <div className="card-image">
           <img src="https://images.unsplash.com/photo-1623509434589-7b70883e8d3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d2FscGFwZXJ8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" />
         </div>
+        <div className="card-content">
+          <i className="material-icons">favourite</i>
+          <h6>Title</h6>
+          <p>It is my first post</p>
+          <input type="text" placeholder="add comment" />
+        </div>
+      </div>
+      <div className="card home_card">
+        <h4>Shehroz</h4>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1623509434589-7b70883e8d3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d2FscGFwZXJ8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" />
+        </div>
+        <div className="card-content">
+          <h6>Title</h6>
+          <p>It is my first post</p>
+          <input type="text" placeholder="add comment" />
+        </div>
       </div>
     </div>
   )
