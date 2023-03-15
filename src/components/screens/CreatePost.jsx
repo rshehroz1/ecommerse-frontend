fb9e0536b31c9e8e22553b723c96b5f13f130a79
Shehroz <raxmatshehroz@gmail.com>
2023-03-15 22:50:37 +0500

    post input

--- src/components/screens/CreatePost.jsx
+++ src/components/screens/CreatePost.jsx
@@ -1,5 +1,17 @@
 export default function CreatePost() {
   return (
-    <div>CreatePost</div>
+    <div className="card input-field">
+        <input type="text" placeholder="title"/>
+        <input type="text" placeholder="body"/>
+        <div class="file-field input-field">
+      <div class="btn">
+        <span>File</span>
+        <input type="file" />
+      </div>
+      <div class="file-path-wrapper">
+        <input class="file-path validate" type="text" />
+      </div>
+    </div>
+    </div>
   )
 }
