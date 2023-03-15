f2d4b6bf85ba8c5c1f82dfc7f66fb73094a51e32
Shehroz <raxmatshehroz@gmail.com>
2023-03-15 23:00:18 +0500

    maqola qoshish

--- src/components/screens/CreatePost.jsx
+++ src/components/screens/CreatePost.jsx
@@ -1,6 +1,7 @@
 export default function CreatePost() {
   return (
     <div className="card input-field post_card">
+        <h2>Maqola</h2>
         <input type="text" placeholder="title"/>
         <input type="text" placeholder="body"/>
         <div class="file-field input-field">
@@ -12,7 +13,7 @@ export default function CreatePost() {
         <input class="file-path validate" type="text" />
       </div>
     </div>
-    <button className="waves-effect waves-light btn #0d47a1 blue darken-3">Maqola qo'shish</button>
+    <button className="waves-effect waves-light btn">Maqola qo'shish</button>
     </div>
   )
 }
