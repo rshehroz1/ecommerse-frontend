8a83bf0e0f5d92eb651e262c46cf84f9b26346e0
Shehroz <raxmatshehroz@gmail.com>
2023-03-15 22:31:49 +0500

    home

--- src/components/screens/Home.jsx
+++ src/components/screens/Home.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className="card home_card">
+        <h4>Shehroz</h4>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1623509434589-7b70883e8d3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8d2FscGFwZXJ8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
