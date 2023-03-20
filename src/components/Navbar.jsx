facad00f67b8e03d79b55664a42b70ee8822ec34
Shehroz <raxmatshehroz@gmail.com>
2023-03-20 10:42:02 +0500

    xatolar tuzatildi2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,10 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <nav className='white'>
-    <div className="nav-wrapper container nav_bg">
+    <nav className="#424242 grey darken-3
+    "style={{height: "10vh"}}>
+    <div className="nav-wrapper container nav_bg #424242 grey darken-3
+">
       <Link to="/" className="brand-logo">Uzbegram</Link>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><Link to="/signin">Signin</Link></li>
