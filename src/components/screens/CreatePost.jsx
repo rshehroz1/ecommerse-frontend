56b32a06ac76126092c3842f7a6e2f3276497ac0
Shehroz <raxmatshehroz@gmail.com>
2023-03-21 16:49:45 +0500

    cloud img

--- src/components/screens/CreatePost.jsx
+++ src/components/screens/CreatePost.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react"
+
 export default function CreatePost() {
+  const [title, setTitile] = useState("")
+  const [body, setBody] = useState("")
+  const [img, setImg] = useState("")
+
+  // const postFetails =()=>{
+
+  // }
   return (
     <div className="card cardPost">
         <div className="card-image">
@@ -9,18 +18,18 @@ export default function CreatePost() {
         <div class="input-field col s6">
           
           <i class="material-icons prefix">subtitles</i>
-          <input id="icon_prefix" type="text" class="validate" />
-          <label for="icon_prefix">Sarlavha</label>
+          <input id="icon_prefix" type="text" class="validate" value={body} onChange={(e)=> setBody(e.target.value)} />
+          <label htmlFor="icon_prefix">Sarlavha</label>
         </div>
         <div class="input-field col s6">
           <i class="material-icons prefix">content_paste</i>
-          <input id="icon_prefix" type="text" class="validate" />
-          <label for="icon_prefix">Maqola</label>
+          <input id="icon_prefix" type="text" class="validate"  value={title} onChange={(e)=> setTitile(e.target.value)} />
+          <label htmlFor="icon_prefix">Maqola</label>
         </div>
         <div class="file-field input-field">
       <div class="btn">
         <span><i className="material-icons">add</i></span>
-        <input type="file" />
+        <input type="file" onChange={(e)=>setImg(e.target.files[0])}/>
       </div>
       <div class="file-path-wrapper">
         <input class="file-path validate" type="text" placeholder="Rasimingizni joylang" />
